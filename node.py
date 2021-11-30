e7254e96cb1f33613ed3577d31823c5a72962da6
Marshblocker <91042310+Marshblocker@users.noreply.github.com>
2021-11-30 17:59:04 +0800

    Initial commit.

---
+++ node.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+from typing import Generic, Optional, TypeVar
+
+
+T = TypeVar("T", int, float, str)
+V = TypeVar("V", int, float, str)
+
+class Node(Generic[T]):
+	def __init__(self, data: T, next: Optional[Node[T]] = None):
+		self.data: T = data
+		self.next: Optional[Node[T]] = next
+	
+
+class BSTNode(Generic[V]):
+	def __init__(self, data: V, 
+				 left: Optional[BSTNode[V]] = None, 
+				 right: Optional[BSTNode[V]] = None):
+		self.data = data
+		self.left = left
+		self.right = right
